f7b3e983fdd267a751660c4dedec43f1c6fa8cd9
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-29 23:36:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,7 +17,6 @@ def assign_rooms(attendees)
   rooms = []
   attendees.each_with_index{|name, index|}
   rooms.push("hello #{name}! You'll be assigned to room #{index}")
-end
   return rooms
 end
 
