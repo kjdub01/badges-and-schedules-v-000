169ba6c6684d10fcd2b271768c6257d62ff4b493
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-29 23:55:19 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,7 +4,7 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
- attendees.map{ |badge| badge_maker(badge) }
+ attendees.map{ |badges| badge_maker(badges) }
 end
 
 def assign_rooms(attendees)
@@ -18,7 +18,8 @@ rooms = []
 end
 
 def printer(attendees)
+  batch_badge_creator(attendees) 
+  puts badges
   
-  puts batch_badge_creator(attendees) 
   
 end
