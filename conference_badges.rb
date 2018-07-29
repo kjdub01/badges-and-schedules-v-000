bb959b758be9269d072cec80668dd3c23c14b121
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-29 23:38:50 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,18 +6,11 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  badges = []
-  attendees.each do |name|
-    badges.push("Hello, my name is #{name}.")
-  end
-  return badges
+ 
 end
 
 def assign_rooms(attendees)
-  rooms = []
-  attendees.each_with_index{|name|}
-  rooms.push("hello #{name}! You'll be assigned to room #{index}")
-  return rooms
+  
 end
 
 def printer
