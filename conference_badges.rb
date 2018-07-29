17ce9fd0c4edbc179f8431946329522087e386c7
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-29 23:14:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,18 @@
 # Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  attendees.collect("Hello, my name is #{}")
+end
+
+def assign_rooms(name)
+  
+end
+
+def printer
+  
+end
